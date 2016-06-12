c2923631bfafa6f567ead71eb640204501794e52
duanxue <1360132676@qq.com>
2016-06-12 23:51:44 +0800

    first commit

---
+++ src/collect_all_even.js
@@ -0,0 +1,14 @@
+'use strict';
+
+function collect_all_even(collection) {
+  //在这里写入代码
+  var num=[],k=0;
+  for(var i=0;i<collection.length;i++)
+    {
+       if(collection[i]%2==0)
+           num[k++]=collection[i];
+    }
+  return num;
+}
+
+module.exports = collect_all_even;
